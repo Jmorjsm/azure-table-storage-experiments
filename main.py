3a13a90d93da9e0cba797afc816b2a89d75e57ea
Jon Morgan <jmorjsm@gmail.com>
2023-02-11 08:18:02 +0000

    Basic upsert

--- main.py
+++ main.py
@@ -1,29 +1,60 @@
-# This is a sample Python script.
+from time import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from azure.data.tables import TableClient, TableTransactionError
+from azure.core.exceptions import ResourceExistsError
 
 
 def generate_entities(start_index: int, count: int):
     e = []
     for i in range(start_index, start_index + count):
-        e += {
-            'start_index': start_index,
-            'current_index': i
-            }
+        e.append({
+            'startIndex': start_index,
+            'currentIndex': i
+            })
     return e
 
 
-def process(e):
-    for i, e in enumerate(e):
+def to_entity(i, e):
+    e["PartitionKey"] = 'basic'
+    e["RowKey"] = str(i)
+    return e
+
+
+def basic_upsert(i, e):
+    table_client = get_table_client('basic')
+    entity = to_entity(i, e)
+    print(entity)
+    table_client.upsert_entity(entity)
+
+
+def process(items):
+    print(items)
+    for i, e in enumerate(items):
+        print(type(e))
+        print(e)
+        basic_upsert(i, e)
         if i % 50 == 0:
             print("processing entity with index %d" % i)
     print("Done, processed a total of %d entities" % len(e))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    entities = generate_entities(0, 1000)
-    process(entities)
+def get_table_client(table_name: str):
+    connection_string = 'UseDevelopmentStorage=true'
+    table_client = TableClient.from_connection_string(conn_str=connection_string,table_name=table_name)
+    try:
+        table_client.create_table()
+        print('table %s created successfully' % table_name)
+    except ResourceExistsError:
+        print('table %s already exists' % table_name)
+    return table_client
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == '__main__':
+    n = 1000
+    entities = generate_entities(0, n)
+    start_time = time()
+    process(entities)
+    execution_time = time() - start_time
+    eps = n/(execution_time*1000)
+    print("Finished saving %i entities in %dms (%d entities per second)" % (n, execution_time, eps))
+
