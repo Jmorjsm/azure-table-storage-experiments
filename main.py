586d5a900af9ade0e130d195dfcc83f38aa9606f
Jon Morgan <jmorjsm@gmail.com>
2023-02-11 09:01:46 +0000

    fix time formatting

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from time import time
+from datetime import datetime
 
-from azure.data.tables import TableClient, TableTransactionError
 from azure.core.exceptions import ResourceExistsError
+from azure.data.tables import TableClient
 
 
 def generate_entities(start_index: int, count: int):
@@ -23,19 +23,15 @@ def to_entity(i, e):
 def basic_upsert(i, e):
     table_client = get_table_client('basic')
     entity = to_entity(i, e)
-    print(entity)
     table_client.upsert_entity(entity)
 
 
 def process(items):
-    print(items)
     for i, e in enumerate(items):
-        print(type(e))
-        print(e)
         basic_upsert(i, e)
         if i % 50 == 0:
             print("processing entity with index %d" % i)
-    print("Done, processed a total of %d entities" % len(e))
+    print("Done, processed a total of %d entities" % len(items))
 
 
 def get_table_client(table_name: str):
@@ -45,16 +41,18 @@ def get_table_client(table_name: str):
         table_client.create_table()
         print('table %s created successfully' % table_name)
     except ResourceExistsError:
-        print('table %s already exists' % table_name)
+        # print('table %s already exists' % table_name)
+        pass
     return table_client
 
 
 if __name__ == '__main__':
     n = 1000
     entities = generate_entities(0, n)
-    start_time = time()
+    start_time = datetime.now()
     process(entities)
-    execution_time = time() - start_time
-    eps = n/(execution_time*1000)
-    print("Finished saving %i entities in %dms (%d entities per second)" % (n, execution_time, eps))
+    execution_time = datetime.now() - start_time
+    eps = float(n) / execution_time.total_seconds()
+    milliseconds = execution_time.microseconds / 1000
+    print(f"Finished saving {n:d} entities in {milliseconds:d}ms ({eps:f} entities per second)")
 
