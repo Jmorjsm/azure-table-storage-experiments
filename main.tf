a593ce515a2c4d11de5ac944ed75a056ffcb1b47
Jon Morgan <jmorjsm@gmail.com>
2023-03-16 21:22:08 +0000

    try and figure out image

--- main.tf
+++ main.tf
@@ -33,15 +33,23 @@ resource "azurerm_container_group" "table_storage_experiments" {
   location            = azurerm_resource_group.table_storage_experiments.location
   resource_group_name = azurerm_resource_group.table_storage_experiments.name
   ip_address_type     = "Public"
-  dns_name_label      = "aci-label"
+  dns_name_label      = "tserunner"
   os_type             = "Linux"
   restart_policy      = "Never"
 
   container {
     name   = "storage-experiment"
-    image  = "jmorjsm/azure-table-storage-experiments"
+    image  = "docker.io/jmorjsm/azure-table-storage-experiments:latest"
     cpu    = "0.5"
     memory = "1.5"
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+
+  image_registry_credential {
+    server   = "docker.io"
   }
 
   tags = {
