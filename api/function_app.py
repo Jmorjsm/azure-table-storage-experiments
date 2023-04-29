70c0eb0f167aac85fa7acb1ed40b7fe2a640cf2f
Jon Morgan <jmorjsm@gmail.com>
2023-04-29 08:51:10 +0100

    add function for getting results from table

--- api/function_app.py
+++ api/function_app.py
@@ -1,14 +1,21 @@
+import json
+import os
+
 import azure.functions as func
+from azure.data.tables import TableClient
 
 app = func.FunctionApp()
-
-@app.function_name(name="HttpTrigger1")
-@app.route(route="hello") # HTTP Trigger
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("HttpTrigger1 function processed a request!!!")
 
 
 @app.function_name(name="results")
 @app.route(route="results")
-def results(req func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("getting the results")
+def results(req: func.HttpRequest) -> func.HttpResponse:
+    connection_string = os.environ.get("STORAGE_CONNECTION", "UseDevelopmentStorage=true")
+    table_name = "results"
+    results_array = []
+    with TableClient.from_connection_string(connection_string, table_name) as table_client:
+        queried_entities = table_client.query_entities()
+        for entity in queried_entities:
+            results_array.append(entity)
+
+    return func.HttpResponse(json.dumps(results_array))
